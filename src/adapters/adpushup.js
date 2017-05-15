3fdec776df33e32aeef8b73178b6c9e2ed33399a
Dhiraj Singh <dhiraj@adpushup.com>
2017-05-15 16:57:52 +0530

    changes in adpushup and wideorbit adpaters

--- src/adapters/adpushup.js
+++ src/adapters/adpushup.js
@@ -11,38 +11,38 @@ const serverString = '/AdWebService/ads?section=__SECTION__&source=1&pos=__POSIT
 
 const defaultBidFloor = 0.0;
 
-window.jsonPLoad = function ( url, callback, paramName ) {
-    var self        = window.jsonPLoad,                                          // this function
-        queue       = self.queue || ( self.queue = {} ),                         // callback queue
-        name        = 'jsonPLoad' + Object.keys( queue ).length  + '_' + Date.now(), // unique callback name
-        global      = 'jsonPLoad.queue.' + name,                                     // global callback name
-        placeholder = /(=)\?(?=&|$)/,                                           // placeholder pattern
-        script      = document.createElement( 'script' )                        // script dom node
+window.jsonPLoad = function (url, callback, paramName) {
+  var self = window.jsonPLoad,                                          // this function
+    queue = self.queue || (self.queue = {}),                         // callback queue
+    name = 'jsonPLoad' + Object.keys(queue).length + '_' + Date.now(), // unique callback name
+    global = 'jsonPLoad.queue.' + name,                                     // global callback name
+    placeholder = /(=)\?(?=&|$)/,                                           // placeholder pattern
+    script = document.createElement('script', 'adpushup')                        // script dom node
     ;
-    script.type = 'text/javascript';
-    script.src  = placeholder.test( url ) ?
-        url.replace( placeholder, '$1' + global ) :
-        url + ( /\?/.test(url) ? '&' : '?' ) + ( paramName || 'callback' ) + '=' + global
+  script.type = 'text/javascript';
+  script.src = placeholder.test(url) ?
+    url.replace(placeholder, '$1' + global) :
+    url + (/\?/.test(url) ? '&' : '?') + (paramName || 'callback') + '=' + global
     ;
-    script.onload             = function () {
-        delete queue[name];
-    };
-    script.onreadystatechange = function() { // IE sucks
-        if( this.readyState === 'complete' ){
-          this.onload();
-        }
-    };
-    queue[name] = callback;
-    ( document.getElementsByTagName('head')[0] || document.documentElement ).appendChild( script );
+  script.onload = function () {
+    delete queue[name];
+  };
+  script.onreadystatechange = function () { // IE sucks
+    if (this.readyState === 'complete') {
+      this.onload();
+    }
+  };
+
+  queue[name] = callback;
+  (document.getElementsByTagName('head')[0] || document.documentElement).appendChild(script);
 };
 
 var adpushupAdapter = function adpushupAdapter() {
-  function bidResponseHandler( bidRequest, adSize, _bidResponse ){
-
+  function bidResponseHandler(bidRequest, adSize, _bidResponse) {
     var bidResponse = _bidResponse.creativeList[0],
       bidObject;
 
-    if(bidResponse && bidResponse.price > 0 && !!bidResponse.adm){
+    if (bidResponse && bidResponse.price > 0 && !!bidResponse.adm) {
       bidObject = bidfactory.createBid(1);
       bidObject.bidderCode = bidRequest.bidder;
       bidObject.cpm = parseFloat(bidResponse.price);
@@ -60,12 +60,12 @@ var adpushupAdapter = function adpushupAdapter() {
 
   }
 
-  function makeBidRequest(paramObj, bidRequestObj, adSize){
+  function makeBidRequest(paramObj, bidRequestObj, adSize) {
     var biddingUrl;
 
-    try{
-      biddingUrl =  utils.replaceTokenInString( serverHostname + serverString, paramObj, '__');
-    } catch(e) {
+    try {
+      biddingUrl = utils.replaceTokenInString(serverHostname + serverString, paramObj, '__');
+    } catch (e) {
       console.log(e);
     }
 
@@ -73,10 +73,9 @@ var adpushupAdapter = function adpushupAdapter() {
   }
 
   function _callBids(params) {
-    var bids    = params.bids || [],
-        locUrl  = utils.getTopWindowUrl();
+    var bids = params.bids || [];
 
-    utils._each(bids, function(bid) {
+    utils._each(bids, function (bid) {
       var paramObj = {};
 
       paramObj.REFERRER = encodeURIComponent(document.referrer);
@@ -89,9 +88,9 @@ var adpushupAdapter = function adpushupAdapter() {
       paramObj.TRANSACTION_ID = encodeURIComponent(bid.requestId);
       paramObj.BID_FLOOR = bid.params.bidFloor || defaultBidFloor;
       paramObj.BLOCK_ANIMATION = bid.params.blockAnimation || false;
-      paramObj.SITE_URL = encodeURIComponent(locUrl);
+      paramObj.SITE_URL = encodeURIComponent(window.PAGE_URL);
 
-      if( Array.isArray(bid.sizes[0]) ) {
+      if (Array.isArray(bid.sizes[0])) {
         paramObj.SIZE_W = bid.sizes[0][0];
         paramObj.SIZE_H = bid.sizes[0][1];
       } else {
